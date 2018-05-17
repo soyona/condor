080fbb640efcd64feae408ebb7f403907a6f97e1
kanglei <kanglei@shopin.cn>
2018-05-17 14:09:44 +0800

    add object sample

---
+++ basic-sample-object/src/main/java/sample/object/ObjectDemo.java
@@ -0,0 +1,14 @@
+package sample.object;
+
+/**
+ * @author soyona
+ * @Package sample.object
+ * @Desc:
+ * @date 2018/5/17 14:07
+ */
+public class ObjectDemo {
+    public static void main(String[] args) {
+        Object o = new Object();
+
+    }
+}

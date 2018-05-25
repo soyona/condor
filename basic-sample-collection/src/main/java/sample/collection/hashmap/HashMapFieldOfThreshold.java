bf4bb5e7329dfcfad67206b76075d2a33e010ad7
kanglei <kanglei@shopin.cn>
2018-05-25 17:28:49 +0800

    分析thredhold变化过程

--- basic-sample-collection/src/main/java/sample/collection/hashmap/HashMapFieldOfThreshold.java
+++ basic-sample-collection/src/main/java/sample/collection/hashmap/HashMapFieldOfThreshold.java
@@ -8,6 +8,26 @@ import java.util.Map;
  * @author soyona
  * @Package sample.collection.hashmap
  * @Desc:
+ * 扩容门槛：threshold
+ * 1、new HashMap()时， threshold=tableSizeFor(initialCapacity)
+ * 2、threshold = (capacity * load factor)
+ * 3、每次扩容时：
+ *      capacity = old_capacity<<1,
+ *      threshold = capacity * DEFAULT_LOAD_FACTOR;
+ *
+ * 场景一：
+ * 构造HashMap时，Map map = new HashMap(3);
+ * table.capacity=4
+ * thredhold=4
+ * next_size=4<<1 =8
+ * map.size=0
+ * 首次添加元素时，map.put("1","hello");
+ * table.capacity=4
+ * thredhold=3
+ * next_size=4<<1 =8
+ * map.size=1
+ *
+ *
  * @date 2018/4/26 12:14
  */
 public class HashMapFieldOfThreshold {
@@ -20,13 +40,19 @@ public class HashMapFieldOfThreshold {
          *
          */
         Map map = new HashMap(3);
-        printField("loadFactor",map);
+//        printField("loadFactor",map);
         printField("threshold",map);
 
         map.put("1","Hello");//初次，发生resize(),之后改变threshold的值
 
-        printField("loadFactor",map);
+//        printField("loadFactor",map);
         printField("threshold",map);
+
+        for (int i = 0; i <50 ; i++) {
+            map.put("1"+i,"Hello");
+            System.out.println("Map.size="+map.size());
+            printField("threshold",map);
+        }
 
     }
 

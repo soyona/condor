1e8a80cfd9208f531163443f036e74388f85944f
kanglei <kanglei@shopin.cn>
2018-05-26 14:05:34 +0800

    HashMap初始化时：size=0，默认initCapacity=12，亦可指定，thredhold=initCapacity 增加扩容过程。

--- basic-sample-collection/src/main/java/sample/collection/hashmap/HashMapFieldOfThreshold.java
+++ basic-sample-collection/src/main/java/sample/collection/hashmap/HashMapFieldOfThreshold.java
@@ -1,5 +1,6 @@
 package sample.collection.hashmap;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,11 +22,16 @@ import java.util.Map;
  * thredhold=4
  * next_size=4<<1 =8
  * map.size=0
+ *
  * 首次添加元素时，map.put("1","hello");
  * table.capacity=4
  * thredhold=3
  * next_size=4<<1 =8
  * map.size=1
+ *
+ *扩容时机：
+ *   if (++size > threshold)
+        resize();
  *
  *
  * @date 2018/4/26 12:14
@@ -39,19 +45,28 @@ public class HashMapFieldOfThreshold {
          *tableSizeFor(3)=4
          *
          */
+
+        //场景一：
+        //第一步：初始化HashMap initialCapacity=3
+        //initialCapacity=3，经过tableSizeFor(initialCapacity)之后获取最近的2的幂次的 数组长度，为4
+        //thredhold初始化为initialCapacity，为3
         Map map = new HashMap(3);
 //        printField("loadFactor",map);
+        System.out.println("初始化HashMap时：===============");
         printField("threshold",map);
+        System.out.println("map.size()="+map.size());
+        printTableLength("table",map);
 
-        map.put("1","Hello");//初次，发生resize(),之后改变threshold的值
-
-//        printField("loadFactor",map);
-        printField("threshold",map);
-
-        for (int i = 0; i <50 ; i++) {
-            map.put("1"+i,"Hello");
-            System.out.println("Map.size="+map.size());
+        //第二步：put第一个元素后
+        for (int i = 1; i <50 ; i++) {
+            map.put("i"+i,"kanglei");
+            if(i==1){
+                System.out.println("put第1个元素之后：发生resize(),之后改变threshold的值");
+            }
+            System.out.println("put第"+i+"个元素之后：===============");
             printField("threshold",map);
+            System.out.println("map.size()="+map.size());
+            printTableLength("table",map);
         }
 
     }
@@ -68,6 +83,39 @@ public class HashMapFieldOfThreshold {
                 System.out.println(field.getName() + ":" + field.get(object) );
             }
         }
+    }
+
+    /**
+     * 获取HashMap.Node[]数组长度
+     * @param fieldName
+     * @param object
+     */
+    public static void printTableLength(String fieldName,Object object){
+        try {
+            Field table= getField(fieldName,object);
+            Object o = table.get(object);
+            if(o!=null){
+                Object[] o_ = (Object[]) o;
+                System.out.println("数组长度="+o_.length);
+            }else{
+                System.out.println("数组长度=0");
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Field getField(String fieldName,Object object) throws IllegalAccessException {
+        Field tmp = null;
+        for (Field field : object.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            if(field.getName().equals(fieldName)){
+               tmp =  field;
+               break;
+            }
+        }
+        return tmp;
     }
 
 }

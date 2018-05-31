c2d83c88ca27ed04b6e2e83dd8c71ddc3c96f19c
kanglei <kanglei@shopin.cn>
2018-05-31 09:52:18 +0800

    autoBoxing: Integer a,b=127 Integer a1,b1=128

---
+++ basic-sample-review/src/main/java/sample/review/integer/IntegerDemo.java
@@ -0,0 +1,28 @@
+package sample.review.integer;
+
+/**
+ * @author soyona
+ * @Package sample.review.integer
+ * @Desc: Integer a = 127 过程
+ * 1、<clinit阶段> 变量 a 初始化 0值，
+ * 1.1、调Integer.getInteger(String nm, int val)
+ * 1.2、Integer.valueOf(int i)
+ * 1.3、IntegerCache.cache[i + (-IntegerCache.low)]
+ * 1.4、分析一下代码：
+ *  if (i >= IntegerCache.low && i <= IntegerCache.high)//IntegerCache.low =-128，IntegerCache.high=127
+ *      return IntegerCache.cache[i + (-IntegerCache.low)];//返回缓存
+ *  return new Integer(i);//否则 new 新对象
+ * @date 2018/5/30 14:22
+ */
+class IntegerCacheDemo {
+
+    public static void main(String[] args) {
+        Integer a = 127;
+        Integer b = 127;
+        System.out.println("a1=127,b1=127 :"+(a == b));
+
+        Integer a1 = 128;
+        Integer b1 = 128;
+        System.out.println("a1=128,b1=128 :"+(a1 == b1));
+    }
+}

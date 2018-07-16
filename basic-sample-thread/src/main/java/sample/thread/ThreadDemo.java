af6b6c593e8eb3f233650e0227cc0da6d1a9d369
kanglei <kanglei@shopin.cn>
2018-07-16 12:16:15 +0800

    Two ways of creating a Thread

--- basic-sample-thread/src/main/java/sample/thread/ThreadDemo.java
+++ basic-sample-thread/src/main/java/sample/thread/ThreadDemo.java
@@ -1,14 +1,48 @@
 package sample.thread;
 
+import org.junit.Test;
+
 /**
  * @author soyona
  * @Package sample.thread
- * @Desc:
+ * @Desc:  Two ways of creating a Thread
  * @date 2018/5/6 19:24
  */
 public class ThreadDemo {
-    public static void main(String[] args) {
-        Thread t = new Thread();
-        t.getState();
+    /**
+     * By implementing the Runnable interface
+     */
+    class MyThread implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("implementing  Runnable interface.");
+        }
     }
+    class MyThread2 extends Thread{
+        @Override
+        public void run() {
+            System.out.println("extending Thread class.");
+        }
+    }
+
+
+    /**
+     * How to start a thread that implementing the Runnable interface
+     */
+    @Test
+    public void excThread(){
+        Thread t = new Thread(new MyThread());
+        t.start();
+    }
+
+    /**
+     * How to start a thread that extending the Thread class
+     */
+    @Test
+    public void excThread2(){
+        Thread t = new MyThread2();
+        t.start();
+    }
+
 }

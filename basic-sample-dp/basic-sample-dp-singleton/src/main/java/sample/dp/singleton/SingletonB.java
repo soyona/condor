43b79efd32a7f07704720c2686fc94a708fb5d08
kanglei <kanglei@shopin.cn>
2018-08-16 17:41:40 +0800

    synchronized singleton

--- basic-sample-dp/basic-sample-dp-singleton/src/main/java/sample/dp/singleton/SingletonB.java
+++ basic-sample-dp/basic-sample-dp-singleton/src/main/java/sample/dp/singleton/SingletonB.java
@@ -7,4 +7,16 @@ package sample.dp.singleton;
  * @date 2018/8/5 16:08
  */
 public class SingletonB {
+    private static volatile SingletonB instance;
+    private SingletonB(){};
+    public static SingletonB getInstance(){
+        if(null == instance){
+            synchronized (SingletonB.class){
+                instance = new SingletonB();
+                return instance;
+            }
+        }else {
+            return instance;
+        }
+    }
 }
